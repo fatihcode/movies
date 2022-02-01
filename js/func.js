e65a7f079e02b6a20b913c8d1d099e0760fa7409
Fatih <95652578+fatihqaz@users.noreply.github.com>
2022-02-01 19:50:42 +0300

    star

--- js/func.js
+++ js/func.js
@@ -1,101 +1,145 @@
+// const publicKey ="pk_qqnsponebxed9indw"
+
+// const mostUrl = "https://imdb-api.com/en/API/MostPopularMovies/k_5tc9m3cl";
+// const comingUrl = "https://imdb-api.com/en/API/ComingSoon/k_5tc9m3cl"
+// const top250Url="https://imdb-api.com/en/API/Top250Movies/k_5tc9m3cl"
+// const reviews = "https://imdb-api.com/en/API/Reviews/k_5tc9m3cl/tt1375666"
+// const fullTitleUrl ="https://imdb-api.com/tr/API/Title/k_5tc9m3cl/"
+
+// https://imdb-api.com/en/API/Title/k_5tc9m3cl/tt1375666/Posters,Ratings,
+
+
+
+//---------------------------------------------------
+
+
+const mostUrl = "./json/most.json"
+const comingUrl = "./json/comicSoon.json"
+const top250Url = "./json/top250.json"
+const reviewsUrl = "./json/reviews.json"
+const fullTitleUrl = "./json/fullTitle.json"
+
+
+//---------------------------------------------------
+
 
 async function imdbJson(url) {
 
     try {
-  
-      let json = await fetch(url);
-      // console.log(json);
-  
-      if (json.ok) {
-        let data = await json.json();
-        // console.log(data);
-        return data
-  
-      }
+
+        let json = await fetch(url);
+        // console.log(json);
+
+        if (json.ok) {
+            let data = await json.json();
+            // console.log(data);
+            return data
+
+        }
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  }
-  
-  
-  
-  
-  //---------------------------------------------------
+}
 
-  
-async function top250Id() {
 
-    let response = await imdbJson(top250Url)
+//---------------------------------------------------
+
+
+async function topId(url) {
+
+    let response = await imdbJson(url)
     let data = response.items
-  
+
     // console.log(data)
     // console.log(data.id)
-  
+
     let id250 = []
-  
+
     data.forEach(item => {
-  
-      id250.push(item.id)
-  
+
+        id250.push(item.id)
+
     });
-  
+
     // console.log(id250)
-  
+
     return id250
-  }
-  
-  top250Id()
-  
-  
-  //----------------------------------------------------
-  
-  
-  function random(val, max) {
-  
+}
+
+topId()
+
+
+//----------------------------------------------------
+
+
+function random(val, max) {
+
     let arr = []
-  
+
     for (let i = 0; i < val; i++) {
-  
-      let rand = Math.floor(Math.random() * max)
-  
-      if (arr.indexOf(rand) == -1) {
-        arr.push(rand)
-      } else {
-        i--
-      }
-  
+
+        let rand = Math.floor(Math.random() * max)
+
+        if (arr.indexOf(rand) == -1) {
+            arr.push(rand)
+        } else {
+            i--
+        }
+
     }
-  
+
     return arr
-  
-  }
-  
-  
-  //----------------------------------------------------
-  
-  
-  function summerize(val, max) {
+
+}
+
+
+//----------------------------------------------------
+
+
+function summerize(val, max) {
     let ozet = []
     let sum = 0
-  
+
     if (val.length <= max) {
-      // console.log(val)
-      return val
+        // console.log(val)
+        return val
     } else {
-      let arr = val.split(" ")
-  
-      for (let i = 0; i < arr.length; i++) {
-  
-        sum += arr[i].length
-        if (sum <= max) {
-          ozet.push(arr[i])
+        let arr = val.split(" ")
+
+        for (let i = 0; i < arr.length; i++) {
+
+            sum += arr[i].length
+            if (sum <= max) {
+                ozet.push(arr[i])
+            }
         }
-      }
-      // console.log(ozet.join(" ") + "...")
-  
-      return ozet.join(" ") + "..."
+        // console.log(ozet.join(" ") + "...")
+
+        return ozet.join(" ") + "..."
     }
-  }
-  
-  
-  //----------------------------------------------------
+}
+
+
+//----------------------------------------------------
+
+
+function star(str) {
+
+    let val = str / 2
+
+    let item = ""
+
+    for (let i = 0; i < Math.floor(val); i++) item += `<i class="bi bi-star-fill"></i>`
+
+    if (Math.round(val) > val) {
+        item += `<i class="bi bi-star-half"></i>`
+        val++
+    }
+
+    for (let i = 0; i < 5 - Math.floor(val); i++) item += `<i class="bi bi-star"></i>`
+
+    return item
+
+}
+
+document.querySelector("#str").innerHTML=star(8.4)
--- js/script.js
+++ js/script.js
@@ -8,46 +8,46 @@ const reviews = document.getElementById("reviews");
 
 
 
-// const mostUrl = "https://imdb-api.com/en/API/MostPopularMovies/k_5tc9m3cl";
-// const comingUrl = "https://imdb-api.com/en/API/ComingSoon/k_5tc9m3cl"
-// const top250Url="https://imdb-api.com/en/API/Top250Movies/k_5tc9m3cl"
-// const reviews = "https://imdb-api.com/en/API/Reviews/k_5tc9m3cl/tt1375666"
-// const fullTitleUrl ="https://imdb-api.com/tr/API/Title/k_5tc9m3cl/tt1375666/Posters,Ratings,"
+async function vision(box) {
 
-const mostUrl = "./json/most.json"
-const comingUrl = "./json/comicSoon.json"
-const top250Url = "./json/top250.json"
-const reviewsUrl = "./json/reviews.json"
-const fullTitleUrl = "./json/fullTitle.json"
+  
+  
+  let id = await topId(mostUrl)
+  console.log(id)
+  
+  let rand = random(box, 100)
 
 
+  
+  console.log(rand[0])
+  console.log(id[rand[0]])
+  
+  let item = ""
 
-//---------------------------------------------------
+  for (let i = 0; i < box; i++) {
+    
+    //let data = await imdbJson(`${fullTitleUrl}${id[rand[i]]}/Posters,Ratings,`)
+    let data = await imdbJson(fullTitleUrl)
 
-
-
-
-
-async function vision() {
-
-
-  let data = await imdbJson(fullTitleUrl)
-
-
-  most.innerHTML = `<div class="col">
+    item += `<div class="col">
                         <div class="card h-100">
                             <a href="https://www.imdb.com/title/${data.id}/" target="_blank">
                                 <img src="${data.image}" class="card-img-top" alt="${data.title}">
                             </a>
                             <div class="card-body">
+                                <h5 class="card-title" alt="${data.imDbRating}">${star(data.imDbRating)}</h5>
                                 <h5 class="card-title">${data.fullTitle}</h5>
                                 <p class="card-text">${summerize(data.plotLocal,300)}</p>
                             </div>
                         </div>
                     </div>`
+  }
+
+  most.innerHTML = item
+
 }
 
-vision()
+vision(3)
 
 
 //---------------------------------------------------
@@ -70,11 +70,14 @@ async function offer(box) {
 
     item += `  <div class="col">
                   <div class="card h-100">
+                  
                       <a href="https://www.imdb.com/title/${data[rand[i]].id}/" target="_blank">
                           <img src="${data[rand[i]].image}" class="card-img" alt="${data[rand[i]].fullTitle}">
                       </a>
-                      <div class="card-body">
-                          <h5 class="card-title">${data[rand[i]].title}</h5>
+                      <div class="card-body text-center">
+                          
+                          <h5 class="text-warning">${star(data[rand[i]].imDbRating)}</h5>
+                          <p class="card-title fs-6">${data[rand[i]].title}</p>
                       </div>
                   </div>
                 </div>`;
@@ -100,22 +103,22 @@ async function reviewFunc(val) {
   console.log(data[0])
 
   let item = ""
-  let rand = random(val,25)
+  let rand = random(val, 25)
 
   for (let i = 0; i < val; i++) {
 
     item += `<div class="col my-2">
-  <a href="${data[rand[i]].reviewLink}" class="list-group-item list-group-item-action h-100 d-flex gap-3 py-3" aria-current="true" target="_blank">
-      <img src="img/pic1.png" height="32px" alt="">
-      <div class="d-flex gap-2 w-100 justify-content-between">
-          <div>
-              <h6 class="mb-0">${data[rand[i]].title}</h6>
-              <p class="mb-0 opacity-75 ">${summerize(data[rand[i]].content,100)}</p>
-          </div>
-          <small class="opacity-50 text-nowrap">${data[rand[i]].rate?data[rand[i]].rate+'/10':''}</small>
-      </div>
-  </a>
-</div>`
+                <a href="${data[rand[i]].reviewLink}" class="list-group-item list-group-item-action h-100 d-flex gap-3 py-3" aria-current="true" target="_blank">
+                    <img src="img/pic${i}.png" height="32px" alt="">
+                    <div class="d-flex gap-2 w-100 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">${data[rand[i]].title}</h6>
+                            <p class="mb-0 opacity-75 ">${summerize(data[rand[i]].content,100)}</p>
+                        </div>
+                        <small class="opacity-50 text-nowrap">${data[rand[i]].rate?data[rand[i]].rate+'/10':''}</small>
+                    </div>
+                </a>
+             </div>`
   }
 
   reviews.innerHTML = item
@@ -130,4 +133,3 @@ reviewFunc(6)
 
 
 //---------------------------------------------------
-
